710164b6ea88134b876dee6273117eb90fbe7e63
Eric Emery <ericemery099@gmail.com>
2023-09-18 21:56:57 +0000

    working on api call

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import axios from 'axios'
 import './App.css';
 
-function App() {
+export default function App() {
 
   function getWeatherData() {
 
@@ -22,6 +22,9 @@ function App() {
     }
 
   return (
+    <>
+    getWeatherData()
+    </>
+        )
 
-
-export default App;
+}
